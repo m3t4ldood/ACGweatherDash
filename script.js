2c1b01b4fc7f2928d6316a1984527feb42e25d4a
anthony gannuscio <m3t4ldood@gmail.com>
2021-02-19 11:20:34 -0700

    updated core files

--- script.js
+++ script.js
@@ -4,22 +4,40 @@ const KELVIN = 273;
 // API KEY
 const key = "0e1fdd775b88f31c074e92ab421cc3df"
 
-function getWeather(city){
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-        fetch(api) 
+function getWeather(cityName){
+    let city = cityName
+    // if(!city){
+        city = document.getElementById("searchval").value
+    // }
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    fetch(api) 
     .then( function(response){
         let data = response.json();
         return data;
     })
 
     .then( function(data){
-        
-        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-        weather.description = data.weather[0].description;
-        weather.iconId = data.weather[0].icon;
-        weather.city = data.sys.country;
-    });
+        var forecast = document.getElementById("forecast")
+        var card = document.createElement("div")
+        card.classList.add("card")
+        var cardBody = document.createElement("div")
+        cardBody.classList.add("card-body")
+        var title = document.createElement("h3")
+        title.classList.add("card-title")
+        title.textContent = data.name
+        var temp = document.createElement("p")
+        temp.classList.add("card-text")
+        temp.textContent = "temp: "+Math.floor(data.main.temp - KELVIN);
+        var image = document.createElement("img")
+        image.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`)
 
+        title.appendChild(image)
+        cardBody.appendChild(title)
+        cardBody.appendChild(temp)
+        card.appendChild(cardBody)
+        forecast.appendChild(card)
+    })
+}
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weatherIcon");
 const tempElement = document.querySelector(".temperatureValue p");
@@ -35,20 +53,20 @@ const locationElement = document.querySelector(".location p");
    city : "London",
    country : "GB"
 };*/
-getCurrentPosition(setPosition, error );
- setPosition( position )
-    position.coords.latitude
-    position.coords.longitude
+//  getCurrentPosition(setPosition, error );
+//  setPosition( position )
+//     position.coords.latitude
+//     position.coords.longitude
     
- error(error)
- error.message
+//  error(error)
+//  error.message
     
-if("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition( setPosition, showError );
-}else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser Doesn't Support Geolocation.</p>"
-}
+// if("geolocation" in navigator){
+//     navigator.geolocation.getCurrentPosition( setPosition, showError );
+// }else{
+//     notificationElement.style.display = "block";
+//     notificationElement.innerHTML = "<p>Browser Doesn't Support Geolocation.</p>"
+// }
 
 function setPosition(position){
     let latitude = position.coords.latitude;
@@ -60,35 +78,36 @@ function showError(error) {
     notificationElement.innerHTML=`<p> ${error.message} </p>`;
 }
 
-tempElement.addEventListener("click", function(){
-    if (weather.temperature.value === undefined) return;
-    if (weather.temperature.unit === "celsius" ){
-       let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-       fahrenheit = Math.floor(fahrenheit);
-       tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
-       weather.temperature.unit = "fahrenheit";
+// tempElement.addEventListener("click", function(){
+//     if (weather.temperature.value === undefined) return;
+//     if (weather.temperature.unit === "celsius" ){
+//        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+//        fahrenheit = Math.floor(fahrenheit);
+//        tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
+//        weather.temperature.unit = "fahrenheit";
        
-    }else{
-        tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
-        weather.temperature.unit = "celsius";
-    }
-});
+//     }else{
+//         tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
+//         weather.temperature.unit = "celsius";
+//     }
+// });
 
 function celsiusToFahrenheit( temperature ){
     /* google how to convert celsius to fahrenheit */
     return ( temperature * 9/5 ) + 32;
 }
 
-function displayWeather(weather){
-iconElement.innerHTML = 
- `<img src="icons/${weather.iconId}.png"/>`;
+// function displayWeather(weather){
+// iconElement.innerHTML = 
+//  `<img src="icons/${weather.iconId}.png"/>`;
  
-tempElement.innerHTML = 
- `${weather.temperature.value} ° <span>C</span>`;
+// tempElement.innerHTML = 
+//  `${weather.temperature.value} ° <span>C</span>`;
  
-descElement.innerHTML =
- `weather.description`;
+// descElement.innerHTML =
+//  `weather.description`;
  
-locationElement.innerHTML = 
-`${weather.city}, ${weather.country}`;
-});
+// locationElement.innerHTML = 
+// `${weather.city}, ${weather.country}`;
+// }
+document.getElementById("searchbtn").onclick=getWeather
